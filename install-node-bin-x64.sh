fcc975af55f34f38bd2d929d9e5adff8990b19dd
EC2 Default User <bbbb55952000@gmail.com>
2014-01-28 01:29:58 +0800

    initial commit

--- install-node-bin-x64.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cwd=`pwd`
-
-wget http://nodejs.org/dist/v0.10.24/node-v0.10.24-linux-x64.tar.gz
-gzip -d node-v0.10.24-linux-x64.tar.gz && tar -xvf node-v0.10.24-linux-x64.tar
-rm -f node-v0.10.24-linux-x64.tar.gz
-
-echo "export PATH=$cwd/node-v0.10.24-linux-x64/bin:$PATH" >> ~/.bashrc
