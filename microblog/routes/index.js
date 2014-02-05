129de72e6d0e5a7de32fda5b7b91debd8277d730
muqq <bbbb55952000@gmail.com>
2014-02-05 16:10:18 +0800

    initial commit

--- microblog/routes/index.js
+++ microblog/routes/index.js
@@ -4,20 +4,34 @@
  */
 
 var User = require('../models/user.js');
+var Post = require('../models/post.js');
 exports.index = function(req, res){
+	Post.get(null, function(err, posts){
+	if (err){
+		post = [];
+		}
   res.render('layout', { title: '首頁',user: req.session.user,success:req.flash('success').toString() });
+});
 };
-
 exports.user = function(req, res) {
-
+res.render('post',{title:'發表',user:req.session.user, success:req.flash('success').toString(), error:req.flash('error').toString()});
 };
 
 exports.post = function(req,res){
-
-};
+var currentUser = req.session.user;
+var post = new Post(currentUser.name, req.body.title, req.body.post);
+	post.save(function(err){
+			if (err){
+				req.flash('error',err);
+				return res.redirect('/');
+				}
+			req.flash('success');
+			req.redirect('/');
+	});
+	};
 
 exports.reg = function(req,res){
-  res.render('reg', {title:'使用者註冊'});
+  res.render('reg', {title:'使用者註冊',user:req.session.user,success:req.flash('success').toString(),error:req.flash('error').toString()});
 
 };
 
